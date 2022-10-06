78c60411e99e4f12a501aeff556cbe486c8b6c29
Maimoona M.Irfan <87384658+maimoonairfan@users.noreply.github.com>
2022-10-06 21:33:08 +0500

    Add files via upload

---
+++ assignment2.dart
@@ -0,0 +1,112 @@
+void main(){
+// Answer to Question 1
+// int length = 5;
+// int breadth = 7;
+// if (length == breadth){
+//   print("It's square");
+// }
+// else{
+//   print("It's a rectangle");
+// }
+
+// Answer to Question 2
+//   int people1 = 21;
+//   int people2 = 10;
+//   int people3 = 15;
+//   if(people1>=people2 && people1>=people3){
+//     print("People1 is oldest");
+//   }
+//   else if(people2>=people1 && people2>=people3){
+//     print("People2 is younger");
+//   }
+//   else if(people3>=people1 && people3>=people2){
+//     print("People2 is younger");
+//   }
+//   else{
+//     print("Both are twins");
+//   }
+
+// Answer to Question 3
+//  int number_of_classes_held = 16;
+//  int number_of_classes_attended = 10;
+//  var attendence = number_of_classes_attended/ number_of_classes_held*100;
+//   if(attendence<75){
+//     print('You are not allowed to sit in exams');
+//   }
+//   else{
+//     print('You are eligible to sit in exam!');
+//   }
+
+// Answer to Question 4
+// int year = 1996;
+// int century_yr = 2000;
+// if(year%4==0 && century_yr%400==0){
+//   print("Its a leap year");
+// }
+// else{
+//   print('Not a leap year');
+// }
+
+// Answer to Question 5
+// num temp = 42;
+// if(temp < 0){
+//   print("Freezing weather");
+// }
+// else if(temp <10){
+//   print('Very Cold weather');
+// }
+// else if(temp <20){
+//   print('Cold weather');
+// }
+// else if(temp <30){
+//   print('Normal in Temp');
+// }
+//   else if(temp <40){
+//   print('Its Hot');
+// }
+//  else if(temp >=40){
+//   print('Its  Very Hot');
+// }
+
+// Answer to Question 6
+// var ch = 'a';
+// if(ch=='a' || ch=='e' || ch=='i'|| ch=='o' || ch=='u'){
+// print("Vowels");
+// }
+// else{
+// print('Consonants');
+// }
+
+// Answer to Question 7
+// var cust_id = 1001;
+// var cust_name = 'James';
+// int unit_consumed=800;
+// double chg;
+// double gramt;
+// double surchg = 0;
+// double netamt;
+// print("Electricity Bill");
+// print('Customer_id: $cust_id\nCustomer_name: $cust_name\nUnit_consumed: $unit_consumed');
+//   if(unit_consumed < 200){
+//     chg = 1.20;
+// }
+//   else if(unit_consumed>=200 && unit_consumed<400){
+//     chg = 1.50;
+//   }
+//   else if(unit_consumed>=400 && unit_consumed<600){
+//     chg = 1.80;
+//   }
+//   else {
+//     chg = 2.00;
+//   }
+//   gramt = unit_consumed*chg;
+//    if (gramt>300){
+// 	    surchg = gramt*15/100.0;
+//      }
+//   netamt = gramt+surchg;
+//    if (netamt  < 100){
+// 	netamt =100;
+//      }
+// print("Amount Charges per unit $chg per unit $gramt");
+// print('Net Amount Paid By the Customer: $netamt');
+}
